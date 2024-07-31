91fe741a3b83747116cd31af6c2a9e61496f833b
Nitya Tiwari <nityasdl002@gmail.com>
2024-07-31 22:46:46 +0530

    fix: code cleanup for basic setup

--- client/prettier.config.cjs
+++ client/prettier.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-    "printWidth": 80,
-    "tabWidth": 4,
-    "useTabs": true,
-    "semi": true,
-    "singleQuote": true,
-    "trailingComma": "es5",
-    "bracketSpacing": true,
-    "arrowParens": "avoid"
-}
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
